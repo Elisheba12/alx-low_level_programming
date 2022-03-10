2d9c24b69debddfcecaefc7036c0e9eb58f2a12e
Elizabeth Salako <waleade602@gmail.com>
2022-03-10 13:04:19 +0000

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/* using puts instead of printf */
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
-int main(void) /* main function that prints out a string */
+int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
