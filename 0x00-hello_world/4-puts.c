e817dad5880c23478ae52c201e27ebe5a8c42e3c
Elizabeth Salako <waleade602@gmail.com>
2022-03-10 12:55:33 +0000

    Hello, Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* using puts instead of printf */
+
+int main(void) /* main function that prints out a string */
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
