93750aa0574e4806508715aa7aeda93ee6e59e4e
Elizabeth Salako <waleade602@gmail.com>
2022-10-05 18:59:19 -0500

    Betty fix

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -12,24 +12,24 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
-    size_t start, end, x;
+	size_t start, end, x;
 
-    if (!array || size == 0)
-        return (-1);
+	if (!array || size == 0)
+		return (-1);
 
-    end = 1;
-    start = 1;
-    while (end < size)
-    {
-        if (value < array[end])
-            break;
-        printf("Value checked array[%lu] = [%d]\n", end, array[end]);
-        start = end;
-        end = end * 2;
-    }
-    x = end < size - 1 ? end : size - 1;
-    printf("Value found between indexes [%lu] and [%lu]\n", start, x);
-    return (help_binary(array, value, end / 2, x));
+	end = 1;
+	start = 1;
+	while (end < size)
+	{
+		if (value < array[end])
+			break;
+		printf("Value checked array[%lu] = [%d]\n", end, array[end]);
+		start = end;
+		end = end * 2;
+	}
+	x = end < size - 1 ? end : size - 1;
+	printf("Value found between indexes [%lu] and [%lu]\n", start, x);
+	return (help_binary(array, value, end / 2, x));
 }
 
 /**
@@ -45,20 +45,20 @@ int exponential_search(int *array, size_t size, int value)
  */
 int help_binary(int *array, int value, size_t lo, size_t hi)
 {
-    size_t mid;
+	size_t mid;
 
-    array_print(array, lo, hi);
-    if (hi == lo && array[lo] != value)
-        return (-1);
+	array_print(array, lo, hi);
+	if (hi == lo && array[lo] != value)
+		return (-1);
 
-    mid = ((hi - lo) / 2) + lo;
-    if (array[mid] == value)
-        return (mid);
-    if (array[mid] > value)
-        return (help_binary(array, value, lo, mid - 1));
-    if (array[mid] < value)
-        return (help_binary(array, value, mid + 1, hi));
-    return (-1);
+	mid = ((hi - lo) / 2) + lo;
+	if (array[mid] == value)
+		return (mid);
+	if (array[mid] > value)
+		return (help_binary(array, value, lo, mid - 1));
+	if (array[mid] < value)
+		return (help_binary(array, value, mid + 1, hi));
+	return (-1);
 }
 
 /**
@@ -69,14 +69,14 @@ int help_binary(int *array, int value, size_t lo, size_t hi)
  */
 void array_print(int *array, size_t lo, size_t hi)
 {
-    size_t i;
+	size_t i;
 
-    printf("Searching in array: ");
-    for (i = lo; i <= hi; i++)
-    {
-        printf("%d", array[i]);
-        if (i < hi)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = lo; i <= hi; i++)
+	{
+		printf("%d", array[i]);
+		if (i < hi)
+			printf(", ");
+	}
+	printf("\n");
 }
